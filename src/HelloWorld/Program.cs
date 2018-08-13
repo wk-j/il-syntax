db3b55e1a3f622808a93d4344e32c104d687b8d7
wk-j <somnuk.wk@gmail.com>
2018-08-14 01:23:26 +0700

    Converted cs to il

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace System.Runtime.CompilerServices {
+
+    [AttributeUsage(AttributeTargets.Method)]
+    internal class CompilerIntrinsicAttribute : Attribute { }
+}
+
+class Program {
+}
+
+namespace HelloWorld {
+    class Program {
+        [CompilerIntrinsic]
+        static void il(params object[] args) { }
+
+        [CompilerIntrinsic]
+        static T il<T>(params object[] args) { return default(T); }
+
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
     }
