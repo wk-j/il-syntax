23aaa1806f0a9c9cd447362b717a3dea9ffa8bc1
wk-j <somnuk.wk@gmail.com>
2018-09-06 14:26:04 +0700

    Windows

---
+++ src/MyFunc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFunc {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+        private string MyFunc(string s) {
+
+            return "";
+        }
+    }
+}
